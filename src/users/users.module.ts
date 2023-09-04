81ba0123bf2972234f90ac69da702289d5680be8
HardKILL <hardkill194@gmail.com>
2023-09-04 17:37:59 -0300

    fix/deleteAccount

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,15 +1,9 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersRepository } from './users.repository';
-import { UsersController } from './users.controller';
-import { AuthModule } from 'src/auth/auth.module';
-import { AuthService } from 'src/auth/auth.service';
-import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports:[JwtModule],
-  providers: [UsersService, UsersRepository, AuthService],
-  exports:[UsersService],
-  controllers: [UsersController]
+  providers: [UsersService, UsersRepository],
+  exports:[UsersService]
 })
 export class UsersModule {}
